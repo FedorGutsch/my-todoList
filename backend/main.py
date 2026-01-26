75a6cdc3b604302c2fe6cff0a84a83bf6b7802a5
Fedor Guchinskii <gucinskijfedor@gmail.com>
2026-01-26 03:13:30 +0300

    Added primitive registration route

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from sqlalchemy import create_engine, text
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+import uvicorn
 
 from .settings import Settings
-
 
 app = FastAPI()
 settings = Settings()
 engine = create_engine(str(settings.postgres_link))
+
+class UserData(BaseModel):
+    email: EmailStr
+    password: str
+
 
 
 @app.get("/users/{user_id}/todos")
@@ -22,3 +29,21 @@ def get_everything(user_id: int):
         return todos
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")
+    
+    
+    
+@app.post("/users/login")
+def add_user(userData: UserData):
+    try:
+        with engine.connect() as connection:
+            result = connection.execute(
+                text("INSERT INTO users(email, password_hash) VALUES(:email, :password)"),
+                {'email': userData.email, 'password': userData.password}
+            )
+        return "User has been created"
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")
+    
+    
+if __name__ == "__main__":
+    uvicorn.run(app)
